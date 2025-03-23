18f50a189e8a78066d9d6a6d694ff781618e1852
amacinho <amac@herdagdelen.com>
2025-03-23 16:24:43 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/snap-flex/sw.js', { scope: '/snap-flex/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
